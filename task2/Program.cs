b853e56b154ac6d90a3f46773288f81517e6c15a
Maria Nosova <mv-nosova@yandex.ru>
2024-03-25 00:12:42 +0300

    умножение

---
+++ task2/Program.cs
@@ -0,0 +1,23 @@
+﻿int a1 = 5;
+int a2 = 6;
+//Console.WriteLine(a1);
+//Console.WriteLine(a2);
+int sum = a1 + a2;
+Console.WriteLine(sum);
+
+long s1 = 6;
+long sum2 = s1 + a1;
+Console.WriteLine(sum2);
+
+double d1 = 5.5;
+double sum3 = d1 + a1;
+Console.WriteLine(sum3);
+
+int product1 = a1 * a2;
+Console.WriteLine(product1);
+
+long product2 = s1 * a1;
+double product3 = d1 * a1;
+Console.WriteLine(product2);
+Console.WriteLine(product3);
+
