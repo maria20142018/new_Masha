a93ca174db46cfa870f3bbd5e91c0a76f1462780
Maria Nosova <mv-nosova@yandex.ru>
2024-03-25 22:51:46 +0300

    add task4

---
+++ task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 6;
+int b = 7;
+int c = 10;
+int d = 4;
+int e = 1;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
