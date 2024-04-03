bbab44f6b374144fdaafd181d70ffbf22eb466d3
Maria Nosova <mv-nosova@yandex.ru>
2024-04-03 23:51:38 +0300

    шутка

---
+++ Массивы/задача1/Program.cs
@@ -0,0 +1,18 @@
+﻿//int[] arr = new int[5]{3,5,2,6,1};
+//Console.WriteLine(arr[0]);
+Console.WriteLine(" Светлана, привет!!!");
+Console.WriteLine("Нажми кнопку 6!!!");
+
+ int N = Convert.ToInt32(Console.ReadLine());
+ if (N == 6){
+    Console.WriteLine("СВЕТА - ПУПКА!! ХА-ХА");
+    
+ }
+else {Console.WriteLine(" Неправильно, Света :(");
+      Console.WriteLine("Нажми кнопку 6!!!");
+int a = Convert.ToInt32(Console.ReadLine());
+ if (a == 6){
+    Console.WriteLine("СВЕТА - ПУПКА!! ХА-ХА");
+
+ }
+}
